fd7eceaa1df57b0c80d5e58a01d2227305fc988c
yoloqin <183275831@qq.com>
2020-03-16 00:32:27 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+       
+        static void Main()
+        {
+            
+            (new Form()).ShowDialog();
+        }
+    }
+}
