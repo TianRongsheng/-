e8ece64770b0a0618887565c9da07f079e91c811
yoloqin <183275831@qq.com>
2020-03-19 14:29:12 +0800

    第3章

--- 02_code/CoreCSharpSamples/MathLib/Calculator.cs
+++ 02_code/CoreCSharpSamples/MathLib/Calculator.cs
@@ -3,7 +3,12 @@
     public class Calculator
     {
         public static double Add(double x, double y) => x + y;
-
+        /// <summary>
+        /// 舰队副司令就是浪费
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public static double Subtract(double x, double y) => x - y;
     }
 }
