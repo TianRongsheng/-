1ca1a531163b14669799616141cc96d59b802252
yoloqin <183275831@qq.com>
2020-03-23 14:53:32 +0800

    隐式继承Object

---
+++ 04_OOSamples/InheritFromObject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InheritFromObject
+{
+    class  Program
+    {
+        static void Main()
+        {
+            // Construct a Point object.
+            var p1 = new Point(1, 2);
+
+            // Make another Point object that is a copy of the first.
+            var p2 = p1.Copy();
+
+            // Make another variable that references the first Point object.
+            var p3 = p1;
+
+            // The line below displays false because p1 and p2 refer to two different objects.
+            Console.WriteLine(Object.ReferenceEquals(p1, p2));
+
+            // The line below displays true because p1 and p2 refer to two different objects that have the same value.
+            Console.WriteLine(Object.Equals(p1, p2));
+
+            // The line below displays true because p1 and p3 refer to one object.
+            Console.WriteLine(Object.ReferenceEquals(p1, p3));
+
+            // The line below displays: p1's value is: (1, 2)
+            Console.WriteLine($"p1's value is: {p1.ToString()}");
+        }
+    }
+
+    // This code example produces the following output:
+    //
+    // False
+    // True
+    // True
+    // p1's value is: (1, 2)
+    //
+}
+
+
+
+
---
+++ 04_OOSamples/InheritFromObject/继承并重写Object方法.md
@@ -0,0 +1,24 @@
+1.  C#中所有类隐式继承 Object ,在 Object 类中定义的每个方法都可用于系统中的所有对象
+
+2.  Object类的方法成员
+    . Equals(Object)	 确定指定的对象是否等于当前对象。
+    . Equals(Object, Object)	 确定指定的对象实例是否被视为相等。
+    . Finalize()	 在垃圾回收将某一对象回收前允许该对象尝试释放资源并执行其他清理操作。
+    . GetHashCode()	 作为默认哈希函数。
+    . GetType()	获取当前实例的 Type。
+    . MemberwiseClone()	创建当前 Object 的浅表副本。
+    . ReferenceEquals(Object, Object)	确定指定的 Object 实例是否是相同的实例。
+    . ToString()	返回表示当前对象的字符串。
+
+3.   派生类可以和重写其中一些方法，其中包括：
+    . Equals-支持对象之间的比较。
+    . Finalize-在自动回收对象之前执行清理操作。
+    . GetHashCode-生成与对象的值相对应的数字以支持使用哈希表。
+    . ToString 制造描述类的实例的用户可读文本字符串。
+
+4.  本例来自微软官网[隐式继承对象](https://docs.microsoft.com/zh-cn/dotnet/api/system.object?view=netcore-3.1)
+
+5.  练习 设计一个Student类，并重写Object部分方法。
+        . 设计学生类的常用属性。
+        . 重写Object类的判等方法Equals，实现两个学生对象的判等。
+        . 重写Object类的字符串转换方法ToString，实现学生对象的属性值转换为格式化字符串的方法。
