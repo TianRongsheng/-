0d4bf548fcd3d775ab95ecbdf129de4bb507da90
yoloqin <183275831@qq.com>
2020-03-26 14:43:04 +0800

    接口

---
+++ 04_OOSamples/UsingInterfaceExamplePrintInfo/CB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingInterfaceExamplePrintInfo
+{
+    class CB:IInfo
+    {
+        public string First;
+        public string Last;
+        public double PersonsAge;
+
+        public string GetAge() => PersonsAge.ToString();
+
+
+        public string GetName() => $"{First} {Last}";
+ 
+    }
+}
