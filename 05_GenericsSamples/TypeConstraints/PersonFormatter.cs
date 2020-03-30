5d73337873c54575788d972967bf1b665ebab585
yoloqin <183275831@qq.com>
2020-03-30 14:06:06 +0800

    泛型约束格式化案例

---
+++ 05_GenericsSamples/TypeConstraints/PersonFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeConstraints
+{
+    class PersonFormatter : IFormatProvider, ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            Person person = arg as Person;
+            switch (format)
+            {
+                case "CH": return $"{person.LastName} {person.FirstName}";
+                case "EN": return $"{person.FirstName} {person.LastName}";
+                default: return $"{person.LastName} {person.FirstName}";
+            }
+        }
+
+      public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter)) return this;
+            return null;
+        }
+    }
+}
