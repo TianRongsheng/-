68503fde75e833fef5606a08d6e553c9444b8b73
yoloqin <183275831@qq.com>
2020-04-03 17:56:30 +0800

    事件注释

--- 08_DelegatesAndEventsSamples/EventThresholdReached/Counter.cs
+++ 08_DelegatesAndEventsSamples/EventThresholdReached/Counter.cs
@@ -17,24 +17,37 @@ namespace EventThresholdReached
         public void Add(int x)
         {
             total += x;
+            
             if (total >= threshold)
             {
+                //阈值到达
+                //1.事件数据参数实例化args
                 ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+                //2.事件参数实例args的属性赋值
                 args.Threshold = threshold;
                 args.TimeReached = DateTime.Now;
-                OnThresholdReached(args);
+                //3.调用触发事件方法，方法名字自定义。约定以On开头。还有时态约束
+                
+                // OnThresholdReached(args);
+
+                //创建一个委托变量handler，受EventHandler<ThresholdReachedEventArgs>约束
+                //handler获得事件ThresholdReached，handler可以在事件发生时调用事件订阅者方法
+                EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
+               //事件订阅者如果不为空
+                if (handler != null)
+                {
+                    //间接调用事件订阅者的事件处理器方法。
+                    handler(this, args);
+                }
             }
         }
 
         protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
         {
-            EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
-            if (handler != null)
-            {
-                handler(this, e);
-            }
+            ThresholdReached?.Invoke(this, e);
         }
 
         public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+
     }
 }
--- 08_DelegatesAndEventsSamples/EventThresholdReached/Program.cs
+++ 08_DelegatesAndEventsSamples/EventThresholdReached/Program.cs
@@ -6,20 +6,25 @@ namespace EventThresholdReached
     {
         static void Main(string[] args)
         {
+            //用一个0-9的随机数初始化一个Counter对象，得到的随机数将作为Counter的阈值threshold
             Counter c = new Counter(new Random().Next(10));
+            //事件订阅：对象c有一个事件ThresholdReached，
+            //当前Program订阅c对象的事件，并且绑定事件处理器c_ThresholdReached;
+            //当c.ThresholdReached事件发生时，调用c_ThresholdReached方法
             c.ThresholdReached += c_ThresholdReached;
 
             Console.WriteLine("press 'a' key to increase total");
             while (Console.ReadKey(true).KeyChar == 'a')
             {
                 Console.WriteLine("adding one");
-                c.Add(1);
+                c.Add(2);
             }
         }
+        //public delegate void EventHandler<TEventArgs>(object sender, TEventArgs e);
         static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
         {
             Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
-          //  Environment.Exit(0);
+            Environment.Exit(0);
         }
 
     }
