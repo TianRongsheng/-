8603041c48edb6fd7386733ae1d9e061e219e7b0
TRS <6569534+TRS12138@user.noreply.gitee.com>
2020-04-20 13:50:36 +0800

    改异步

--- 15_AsyncSamples/DownloadFile/FileHandler.cs
+++ 15_AsyncSamples/DownloadFile/FileHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DownloadFile
 {
@@ -11,7 +12,7 @@ namespace DownloadFile
         /// <summary>
         /// 文件下载目录
         /// </summary>
-        private static string _directory = @"D:\2020SE\";
+        private static string _directory = @"F:\作业\Test\";
 
         /// <summary>
         /// 下载文件
@@ -21,19 +22,31 @@ namespace DownloadFile
         public static string DownloadFile(string url)
         {
 
-                string fileName = CreateFileName(url);
+            string fileName = CreateFileName(url);
+            WebClient client = new WebClient();
+            client.DownloadFile(url, _directory + fileName);
+            return fileName;
+        }
+        //异步调用
+        public static async Task AsyncDownloadFile(string url)
+        {
+            Console.WriteLine(nameof(DownloadFile));
+            //将指定的工作放在线程池中运行，并返回一个任务
+            await Task.Run(() =>
+            {
                 WebClient client = new WebClient();
+                string fileName = CreateFileName(url);
                 client.DownloadFile(url, _directory + fileName);
                 return fileName;
+            });
         }
-
         /// <summary>
         /// 创建文件名称
         /// </summary>
         public static string CreateFileName(string url)
         {
-            string fileName ;
-            string fileExt = url.Substring(url.LastIndexOf(".")).Trim().ToLower();
+            string fileName;
+            string fileExt = url.Substring(url.LastIndexOf(".")).Trim().ToLower();//ToLower返回此字符串转换为小写形式的副本。
             Random rnd = new Random();
             fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + rnd.Next(10, 99).ToString() + fileExt;
             return fileName;
--- 15_AsyncSamples/DownloadFile/Program.cs
+++ 15_AsyncSamples/DownloadFile/Program.cs
@@ -8,10 +8,10 @@ namespace DownloadFile
     {
         static void Main(string[] args)
         {
-               string url = "http://www.lzzy.net/upload/main/image/2018/09/12/201809121022432155.jpg";
-               string fileName = FileHandler.DownloadFile(url);
-               Console.WriteLine("文件下载成功，文件名称：" + fileName);  
-               Console.ReadLine();
+            string url = "http://www.lzzy.net/upload/main/image/2018/09/12/201809121022432155.jpg";
+            var fileName = FileHandler.AsyncDownloadFile(url);
+            Console.WriteLine("文件下载成功，文件名称：" + fileName.ToString());
+            Console.ReadLine();
         }
     }
 }
